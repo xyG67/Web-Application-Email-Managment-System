1bc91db56b9b7f3632cb46b964976051ad46edda
xyG67 <gxy7272@outlook.com>
2018-02-17 17:57:26 -0800

    add client server

--- index.js
+++ index.js
@@ -1,32 +1,25 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport')
 const keys = require('./config/keys');
-
+mongoose.connect(keys.mongoURI);
+require('./models/User');
+require('./services/passport');
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge:24*60*60*1000,    //1 day
+        keys: [keys.cookieKey]
+    })
+)
+app.use(passport.initialize());
+app.use(passport.session());
 
 //console.developers.google.com
 
-passport.use(
-    new GoogleStrategy({
-        clientID:keys.googleClientID,
-        clientSecret:keys.googleClientSecret,
-        callbackURL:'/auth/google/callback'
-    },(accessToken, refreshToken, profile, done)=>{
-        console.log('access token', accessToken);
-        console.log('refresh token', refreshToken);
-        console.log('profile: ', profile);
-    }
-));
-
-app.get(
-    '/auth/google', 
-    passport.authenticate('google', {
-        scope: ['profile', 'email']
-    })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
